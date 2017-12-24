3650d204d0208f706d2ff1242b74aa70ca8e00b9
msolorio <msolorio789@gmail.com>
2017-12-24 14:45:27 -0800

    add test routes

--- server.js
+++ server.js
@@ -3,7 +3,30 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello express application');
+  res.send('<h2>home page</h2>');
+});
+
+app.get('/about', (req, res) => {
+  res.json({
+    companyName: 'Sam\'s Jams',
+    inventory: [
+      {
+        name: 'Strawberry Jam',
+        ammount: 3
+      },
+      {
+        name: 'Grape Jelly',
+        ammount: 2
+      }
+    ],
+    location: '123 Sweet Street'
+  });
+});
+
+app.get('/notavailable', (req, res) => {
+  res.status(404).json({
+    errorMessage: 'route not available'
+  });
 });
 
 app.listen(3000);
